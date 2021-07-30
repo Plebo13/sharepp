a0228dc8d6bd91b549858e74d47788ad7c6588a7
Lukas Brauckmann <lukas.brauckmann@gmail.com>
2021-07-30 19:50:48 +0200

    Fix new onvista parsing

--- sharepp/__init__.py
+++ sharepp/__init__.py
@@ -1,25 +1,12 @@
-from bs4 import BeautifulSoup
-import requests
 import sys
+import requests
 
 onvista_url = "https://www.onvista.de/"
 
 
 def parse_price(isin):
     response = requests.get(onvista_url + isin)
-    parsed_html = BeautifulSoup(response.text, "html.parser")
-    try:
-        # Single share.
-        price_span = parsed_html.find("ul", class_="KURSDATEN").find("span")
-        price_string = price_span.text
-    except AttributeError:
-        # ETF.
-        column_div = parsed_html.find("div", class_="col col--sm-4 col--md-4 col--lg-4 col--xl-4")
-        price_span = column_div.find("data", class_="text-nowrap")
-        price_string = price_span.text.split(" ")[0]
-
-    price_string = price_string.replace('.', '').replace(',', '.')
-
+    price_string = response.text.split("ask")[1].split(":")[1].split(",")[0]
     return float(price_string)
 
 
