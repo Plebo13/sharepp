55c9b5930e8ba0bf49927fb218b6bd19fff77258
Lukas Brauckmann <lukas.brauckmann@gmail.com>
2021-08-04 22:23:10 +0200

    Rename constant

--- sharepp/__init__.py
+++ sharepp/__init__.py
@@ -1,12 +1,12 @@
 """SharePriceProvider is a small application that provides the current price in EUR for a given ISIN."""
 import requests
 
-onvista_url = "https://www.onvista.de/"
+ONVISTA_URL = "https://www.onvista.de/"
 
 
 def parse_price(isin):
     if isinstance(isin, str):
-        response = requests.get(onvista_url + isin)
+        response = requests.get(ONVISTA_URL + isin)
         price_string = response.text.split("ask")[1].split(":")[1].split(",")[0]
         return float(price_string)
     else:
