fb33f3cc79386de355f4a78bc93a0eacd9948521
Lukas Brauckmann <lukas.brauckmann@gmail.com>
2021-11-26 14:11:15 +0100

    Update requirements.

--- setup.py
+++ setup.py
@@ -4,22 +4,25 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='sharepp',
-    version='1.0',
+    name="sharepp",
+    version="1.0",
     author="Lukas Brauckmann",
     author_email="lukas.brauckmann@gmail.com",
     description="Library for getting ETF and cryptocurrency prices.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Plebo13/sharepp",
-    packages=setuptools.find_packages(where="sharepp"),
+    license="Apache License 2.0",
+    packages=["sharepp"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'requests',
-        'bs4',
+        "enum",
+        "re",
+        "requests",
+        "bs4"
     ],
 )
