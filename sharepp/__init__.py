e2b7150736011059b78e13a3ec8937144c75c43c
Lukas Brauckmann <lukas.brauckmann@gmail.com>
2023-04-08 15:02:00 +0200

    Poetry (#16)
    * Introduce poetry
    
    * Restructure
    
    * Update README.md
    
    * Add script
    
    * Exception handling

--- sharepp/__init__.py
+++ sharepp/__init__.py
@@ -1,4 +1,82 @@
 """SharePriceProvider is a small application that provides the current price
 in EUR for a given ISIN. """
-import sharepp.SharePP
-from sharepp.Coin import Coin
+from enum import Enum
+import re
+import requests
+from bs4 import BeautifulSoup
+
+LANG_UND_SCHWARZ_ETF_URL = "https://www.ls-tc.de/de/etf/"
+COIN_GECKO_URL = (
+    "https://api.coingecko.com/api/v3/simple/price?ids={coin}&vs_currencies={currency}"
+)
+EURO_CURRENCY = "eur"
+
+
+class SharePPError(Exception):
+    pass
+
+
+class Coin(Enum):
+    """Enum representing all currently supported cryptocurrencies."""
+
+    BITCOIN = "bitcoin"
+    ETHEREUM = "ethereum"
+    BINANCE_COIN = "binancecoin"
+    TETHER = "tether"
+    SOLANA = "solana"
+    CARDANO = "cardano"
+    RIPPLE = "ripple"
+    USD_COIN = "usd-coin"
+    POLKADOT = "polkadot"
+    DOGECOIN = "dogecoin"
+
+
+def get_etf_price(isin: str, rounded=False) -> float:
+    """
+    Gets the current price in euro of a given ETF.
+
+    :param isin: the ISIN of the ETF
+    :return: the current price
+    """
+    if not is_isin(isin):
+        raise SharePPError(
+            "You must provide a string object representing a valid ISIN!"
+        )
+
+    response = requests.get(LANG_UND_SCHWARZ_ETF_URL + isin)
+    if response.status_code != 200:
+        raise SharePPError(f"No price information for ISIN {isin} could be found!")
+
+    parsed_html = BeautifulSoup(response.text, "html.parser")
+    price_span = parsed_html.find("div", class_="mono").find("span")
+    price_string = price_span.text.replace(".", "").replace(",", ".")
+
+    if rounded:
+        return round(float(price_string), 2)
+    return float(price_string)
+
+
+def get_coin_price(coin: Coin) -> float:
+    """
+    Gets the current price in euro of a given cryptocurrency.
+
+    :param coin: the cryptocurrency
+    :return: the current price of the cryptocurrency
+    """
+    response = requests.get(
+        COIN_GECKO_URL.format(coin=coin.value, currency=EURO_CURRENCY)
+    ).json()
+    return float(response[coin.value][EURO_CURRENCY])
+
+
+def is_isin(isin: str) -> bool:
+    """
+    Checks whether a string is a valid ISIN or not.
+
+    :param isin: the string to be checked
+    :return: true if the given string is a valid ISIN, otherwise false
+    """
+    if re.match("^[A-Za-z]{2}[A-Za-z0-9]{10}", isin):
+        return True
+    else:
+        return False
--- tests/test_sharepp.py
+++ tests/test_sharepp.py
@@ -1,27 +1,25 @@
 import unittest
-from sharepp import SharePP, Coin
+import sharepp
 
 
 class SharePPTest(unittest.TestCase):
-
     def test_valid_input(self):
-        price = SharePP.get_etf_price("LU1781541179")
+        price = sharepp.get_etf_price("LU1781541179")
         self.assertTrue(float, type(price))
 
     def test_invalid_input(self):
-        try:
-            SharePP.get_etf_price("invalid_isin")
-            self.fail("Expected exception not thrown!")
-        except ValueError as e:
+        with self.assertRaises(ValueError) as e:
+            sharepp.get_etf_price("invalid_isin")
             self.assertEqual(
                 "You must provide a string object representing a valid ISIN!",
-                str(e))
+                str(e.exception),
+            )
 
     def test_get_coin_price(self):
-        for coin in Coin:
-            price = SharePP.get_coin_price(coin)
+        for coin in sharepp.Coin:
+            price = sharepp.get_coin_price(coin)
             self.assertTrue(float, type(price))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
